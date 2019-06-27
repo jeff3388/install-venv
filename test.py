7e03cdfea8dc4801982f9a25a9a27567dca6f292
jojo <jaxlouder@gmail.com>
2019-06-27 14:37:18 +0800

    1234

--- test.py
+++ test.py
@@ -7,8 +7,7 @@ time.sleep(t)
 req = requests.Session()
 url = 'http://temp.check-article.cfd888.info/write_csv'
 
-# 查詢參數
-data = {'string': '公司,電話,地址,na,na,na,公司,電話,地址,na,na,na'}
+data = {'string': 'company,phone,address,na,na,na,company,phone,address,na,na,na'}
 
 r = req.post(url=url, data=data, timeout=5)
 print(r.text)
