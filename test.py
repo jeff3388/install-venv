d46b9f1e84668cac4702e6888988e85266c80fe8
jojo <jaxlouder@gmail.com>
2019-07-11 12:37:19 +0800

    5666

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def get_mac_address():
     return ":".join([mac[e:e+2] for e in range(0,11,2)])
 
 datetime_dt = datetime.datetime.today()
-datetime_str = datetime_dt.strftime("%Y-%m-%d") # today date transfer to sting format
+datetime_str = datetime_dt.strftime("%Y-%m-%d %H:%M:%S") # today date transfer to sting format
 
 # post api
 url = 'http://temp.check-article.cfd888.info/test_csv'
