746b142e51377a5bfa9d5609637ad6189335eea0
jojo <jaxlouder@gmail.com>
2019-07-22 10:50:58 +0800

    '1231'

--- pi-selenium.py
+++ pi-selenium.py
@@ -1,14 +1,21 @@
-from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from pyvirtualdisplay import Display
+from selenium import webdriver
+import os
 
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-profile = webdriver.FirefoxProfile()
-driver = webdriver.Firefox(profile)
+chromedriver='/usr/lib/chromium-browser/chromedriver'
+chrome_options=webdriver.ChromeOptions()
+chrome_options.add_experimental_option("debuggerAddress",  "localhost:9222")
+os.environ["webdriver.chrome.driver"]=chromedriver
+driver = webdriver.Chrome(executable_path=chromedriver)
 
 driver.get("http://www.binarymanaic.com")
 
 print(driver.title)
 
-driver = webdriver.Firefox()
+driver.close()
+driver.quit()
+
--- test.sh
+++ test.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install iceweasel
-sudo apt-get install xvfb
-sudo apt-get install xserver-xephyr
-sudo pip3 install selenium==2.53.6
-sudo pip3 install pyvirtualdisplay
-sudo pip3 install pytest
-wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-arm7hf.tar.gz
-tar -xf geckodriver-v0.19.1-arm7hf.tar.gz
-rm geckodriver-v0.19.1-arm7hf.tar.gz
-sudo chmod a+x geckodriver
-sudo mv geckodriver /usr/local/bin/
+sudo apt-get install xvfb python-pip
+sudo pip install pyvirtualdisplay
+wget http://launchpadlibrarian.net/361669488/chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
+sudo dpkg -i chromium-chromedriver_65.0.3325.181-0ubuntu0.14.04.1_armhf.deb
