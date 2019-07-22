1d1e208dd7820e989fab5108f65880f0b2a6c937
jojo <jaxlouder@gmail.com>
2019-07-22 09:47:03 +0800

    '12'

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-/usr/bin/python3 /jojo/test.py
-echo "hello pi~" >> /home/testboot.txt
+sudo apt-get install iceweasel
+sudo apt-get install xvfb
+sudo apt-get install xserver-xephyr
+sudo pip3 install selenium==2.53.6
+sudo pip3 install pyvirtualdisplay
+sudo pip3 install pytest
+wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-arm7hf.tar.gz
+tar -xf geckodriver-v0.19.1-arm7hf.tar.gz
+rm geckodriver-v0.19.1-arm7hf.tar.gz
+sudo chmod a+x geckodriver
+sudo mv geckodriver /usr/local/bin/
